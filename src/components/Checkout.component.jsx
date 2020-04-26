c8e1ecffee89125b5f15da428f3be2f69d595264
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-04-26 23:36:27 +0545

    Quantity alter toggle

--- src/components/Checkout.component.jsx
+++ src/components/Checkout.component.jsx
@@ -4,14 +4,20 @@ import { createStructuredSelector } from 'reselect'
 import { selectCartArray } from '../reducers/CartItems/cart.selectors'
 import { connect } from 'react-redux'
 
-import { ProductImage } from '../styles/Checkout.styles'
+import { ProductImage, TableData } from '../styles/Checkout.styles'
 
 class Checkout extends Component {
   render() {
     const { sortedArray } = this.props
+    const middleAlign = { verticalAlign: 'middle' }
+    const total = sortedArray.reduce(
+      (accumulator, currvalue) =>
+        accumulator + currvalue.quantity * currvalue.item.price,
+      0
+    )
     return (
       <div className="container">
-        <h3 className="text-center"> Checkout</h3>
+        <h3> Checkout</h3>
         <table class="table table-hover">
           <thead class="thead-dark">
             <tr>
@@ -19,37 +25,48 @@ class Checkout extends Component {
               <th scope="colz">Product</th>
               <th scope="col">Description</th>
               <th scope="col">Price</th>
-              <th scope="col">Quantity</th>
+              <th scope="col" className="text-center">
+                Quantity
+              </th>
               <th scope="col">Total</th>
-              <th scope="col">Remove</th>
+              <th scope="col" className="text-center">
+                Remove
+              </th>
             </tr>
           </thead>
           <tbody>
             {sortedArray.map((item, index) => (
               <tr>
-                <th scope="row">{index}</th>
-                <td>
+                <th scope="row" style={middleAlign}>
+                  {index}
+                </th>
+                <TableData style={middleAlign}>
                   <ProductImage src={item.item.imageUrl} alt="productImage" />
-                </td>
-                <td>
+                </TableData>
+                <TableData style={middleAlign}>
                   <h5>{item.item.name}</h5>
-                </td>
-                <td>
+                </TableData>
+                <TableData style={middleAlign}>
                   <h5>$ {item.item.price}</h5>
-                </td>
-                <td>
-                  <h5>{item.quantity}</h5>
-                </td>
-                <td>
-                  <h5>
-                    $ {parseInt(item.quantity) * parseFloat(item.item.price)}
-                  </h5>
-                </td>
-                <td></td>
+                </TableData>
+                <TableData style={middleAlign}>
+                  <div className="w-100 d-flex justify-content-around">
+                    <button className="btn btn-dark ">-</button>
+                    <h5>{item.quantity}</h5>
+                    <button className="btn btn-dark ">+</button>
+                  </div>
+                </TableData>
+                <TableData style={middleAlign}>
+                  <h5>$ {item.quantity * item.item.price}</h5>
+                </TableData>
+                <TableData style={middleAlign}>
+                  <button className="btn btn-dark w-100">Remove</button>{' '}
+                </TableData>
               </tr>
             ))}
           </tbody>
         </table>
+        <h4 className="text-right p-3">Total= {total}</h4>
       </div>
     )
   }
--- src/styles/Checkout.styles.jsx
+++ src/styles/Checkout.styles.jsx
@@ -3,3 +3,6 @@ import styled from 'styled-components'
 export const ProductImage = styled.img`
   width: 5rem;
 `
+export const TableData = styled.td`
+  vertical-align: middle;
+`
