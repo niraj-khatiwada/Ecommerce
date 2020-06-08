f71fe07d7d1cd926d1517990250bb56bf41b5991
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-06-08 11:17:18 +0545

    Refactored

--- src/Datas/ListItem.datas.jsx
+++ src/Datas/ListItem.datas.jsx
@@ -1,27 +1,32 @@
 export default [
-    {
-      title: 'hats',
-      imageUrl: 'https://images.unsplash.com/photo-1534215754734-18e55d13e346?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-      id: 1
-    },
-    {
-      title: 'jackets',
-      imageUrl: 'https://images.unsplash.com/photo-1551488831-00ddcb6c6bd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-      id: 2
-    },
-    {
-      title: 'sneakers',
-      imageUrl: 'https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-      id: 3
-    },
-    {
-      title: 'womens',
-      imageUrl: 'https://images.unsplash.com/photo-1502323777036-f29e3972d82f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-      id: 4,
-    },
-    {
-      title: 'mens',
-      imageUrl: 'https://images.unsplash.com/photo-1505022610485-0249ba5b3675?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-      id: 5,
-    }
-  ]
+  {
+    title: 'hats',
+    imageUrl:
+      'https://images.unsplash.com/photo-1534215754734-18e55d13e346?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    id: 1,
+  },
+  {
+    title: 'jackets',
+    imageUrl:
+      'https://images.unsplash.com/photo-1551488831-00ddcb6c6bd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    id: 2,
+  },
+  {
+    title: 'sneakers',
+    imageUrl:
+      'https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    id: 3,
+  },
+  {
+    title: 'womens',
+    imageUrl:
+      'https://biowikis.com/wp-content/uploads/2018/12/Shrinkhala-Khatiwada-Bio-Wiki-Net-Worth.jpg',
+    id: 4,
+  },
+  {
+    title: 'mens',
+    imageUrl:
+      'https://images.unsplash.com/photo-1505022610485-0249ba5b3675?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    id: 5,
+  },
+]
